1943ad1cd2232168ab61768b7f7067a5a7c02b93
xadh00m <migeissler@gmail.com>
2014-06-11 11:13:23 +0200

    Update configuration to match current setup

--- configure.sh
+++ configure.sh
@@ -2,4 +2,4 @@
 
 # Configure these variables according to your setup
 # Puppet will read these facter variables
-export FACTER_jenkins_cert=/etc/ssl/certs/jenkins.hoccer.de.crt
+export FACTER_jenkins_cert=/etc/ssl/private/star.hoccer.de.pem
